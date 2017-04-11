913e28cc8e9b1ff487eaf403c3c5b97cb4056e40
Sacha Bron <sacha.bron@copperflake.com>
2017-04-11 16:52:52 +0200

    Working version

--- arduino-status-signal.ino
+++ arduino-status-signal.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
 ESP8266WebServer server(80);
 
@@ -10,6 +11,8 @@ char* STATUS_URL = "http://fixme.ch/status.json";
 int RED = D5;
 int YELLOW = D6;
 int GREEN = D7;
+
+bool isOpen = false;
 
 void setup() {
   pinMode(RED, OUTPUT);
@@ -32,8 +35,6 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   launchServer();
-
-  getRequest(STATUS_URL);
 }
 
 void launchServer() {
@@ -51,19 +52,38 @@ void launchServer() {
 }
 
 void getRequest(char* url) {
-  delay(5000);
   HTTPClient http;
   http.begin(url);
+  
   int httpCode = http.GET();
+  
   // httpCode will be negative on error
   if(httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      Serial.printf("Response code: %d\n", httpCode);
 
       // file found at server
       if(httpCode == HTTP_CODE_OK) {
-          String payload = http.getString();
-          Serial.println(payload);
+          String payloadStr = http.getString();
+          // DEBUG: Serial.println(payloadStr);
+
+          int len = payloadStr.length() + 1;
+          char payload[len];
+          payloadStr.toCharArray(payload, len);
+
+          // JSON Decode
+          DynamicJsonBuffer jsonBuffer(2000);
+          // StaticJsonBuffer<2000> jsonBuffer;
+          JsonObject& status = jsonBuffer.parseObject(payload);
+          
+          if (!status.success()) {
+            Serial.println("parseObject() failed");
+            return;
+          }
+          
+          isOpen = status["state"]["open"];
+          Serial.print("isOpen: ");
+          Serial.println((isOpen) ? "true" : "false");
       }
   } else {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
@@ -106,7 +126,20 @@ void sendOK() {
   server.send(200, "text/plain", "OK");
 }
 
+void updateStatus() {
+  getRequest(STATUS_URL);
+  if (isOpen) {
+    setOpen();
+  } else {
+    setClose();
+  }
+}
+
 void loop() {
+  delay(5000);
+  Serial.println("--------");
+  
   server.handleClient();
+  updateStatus();
 }
 
