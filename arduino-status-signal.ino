3d6e5d9bc3f9c2efcb68a7f5ab1f35f3c74fe346
Sacha Bron <sacha.bron@copperflake.com>
2017-04-11 14:22:10 +0200

    Get status.json from fixme.ch

--- arduino-status-signal.ino
+++ arduino-status-signal.ino
@@ -1,7 +1,11 @@
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
 
 ESP8266WebServer server(80);
+
+WiFiClient client;
+char* STATUS_URL = "http://fixme.ch/status.json";
 
 int RED = D5;
 int YELLOW = D6;
@@ -12,7 +16,7 @@ void setup() {
   pinMode(YELLOW, OUTPUT);
   pinMode(GREEN, OUTPUT);
   
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
 
   Serial.print("Setting soft-AP ... ");
@@ -28,6 +32,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   launchServer();
+
+  getRequest(STATUS_URL);
 }
 
 void launchServer() {
@@ -42,6 +48,28 @@ void launchServer() {
   server.begin();
   
   Serial.println("Web server ready!");
+}
+
+void getRequest(char* url) {
+  delay(5000);
+  HTTPClient http;
+  http.begin(url);
+  int httpCode = http.GET();
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+          String payload = http.getString();
+          Serial.println(payload);
+      }
+  } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
 
 void setOpen() {
